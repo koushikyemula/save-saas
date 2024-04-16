bc209a9d9321fc357e7ef4c7c8ab1e0489cc4c9b
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-04-16 23:24:36 +0530

    chore: more gradient circles

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -9,6 +9,9 @@ export default function Home() {
       <div className="flex h-full flex-col items-center justify-between text-center">
         <LandingHero />
       </div>
+      <BlurryCircle className="absolute -top-2 right-[320px]  hidden bg-[#FFECBB] dark:bg-[#FFECBB]/40 md:block" />
+      <BlurryCircle className="absolute -bottom-6 left-6 hidden bg-[#FFECBB]/50 dark:bg-[#FFECBB]/20 md:block" />
+      <BlurryCircle className="absolute bottom-[-60px] right-0 -z-10 hidden bg-[#3633D0]/5 dark:bg-[#3633D0]/10 md:block" />
     </section>
   );
 }
