612bb08516012a732851be4f97afe628f65c00f2
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2024-05-26 23:26:14 +0530

    chore: replacing blurry circles with an animated grid

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/lib/utils";
+import GridPattern from "@/components/gradients/animated-grid-pattern";
 import Header from "@/components/header/header";
 
 export default function RootLayout({
@@ -6,7 +8,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-full">
+    <div className="relative h-full">
+      <GridPattern
+        numSquares={30}
+        maxOpacity={0.5}
+        duration={3}
+        repeatDelay={1}
+        className={cn(
+          "[mask-image:radial-gradient(300px_circle_at_center,white,transparent)]",
+          "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12"
+        )}
+      />
       <Header />
       <main>{children}</main>
     </div>
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,17 +1,14 @@
+import { cn } from "@/lib/utils";
+import GridPattern from "@/components/gradients/animated-grid-pattern";
 import { BlurryCircle } from "@/components/gradients/blurry-circle";
 import { LandingHero } from "@/components/hero";
 
 export default function Home() {
   return (
     <section className="relative mt-4 flex h-full flex-col px-8">
-      <BlurryCircle className="absolute right-10 top-[90%] -z-10 hidden bg-[#F59F95]/30 dark:bg-[#F59F95]/10 md:block" />
-      <BlurryCircle className="absolute left-10 top-[90%] -z-10 hidden bg-[#3633D0]/10 dark:bg-[#3633D0]/10 md:block" />
       <div className="flex h-full flex-col items-center justify-between text-center">
         <LandingHero />
       </div>
-      <BlurryCircle className="absolute -top-16 right-[320px] hidden bg-[#FFECBB] dark:bg-[#FFECBB]/40 md:block" />
-      <BlurryCircle className="absolute -bottom-6 left-6 hidden bg-[#FFECBB]/50 dark:bg-[#FFECBB]/20 md:block" />
-      <BlurryCircle className="absolute bottom-[-60px] right-0 -z-10 hidden bg-[#3633D0]/5 dark:bg-[#3633D0]/10 md:block" />
     </section>
   );
 }
